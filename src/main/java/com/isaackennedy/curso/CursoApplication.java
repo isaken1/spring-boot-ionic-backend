64a39be9155c49523f5ad429ca56e568adc478f7
isaac_kennedy@hotmail.com <isaac_kennedy@hotmail.com>
2018-10-10 20:21:46 -0300

    Adicionando operação de instanciação

--- src/main/java/com/isaackennedy/curso/CursoApplication.java
+++ src/main/java/com/isaackennedy/curso/CursoApplication.java
@@ -1,12 +1,30 @@
 package com.isaackennedy.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.isaackennedy.curso.domain.Categoria;
+import com.isaackennedy.curso.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository catRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
