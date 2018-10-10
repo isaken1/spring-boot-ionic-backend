2e990edb57b54eb8bfb25c42227f1eca9237bb8a
isaac_kennedy@hotmail.com <isaac_kennedy@hotmail.com>
2018-10-10 20:46:33 -0300

    Criação do domínio de produtos e associação n->n com o domínio de categorias

--- src/main/java/com/isaackennedy/curso/Application.java
+++ src/main/java/com/isaackennedy/curso/Application.java
@@ -8,23 +8,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.isaackennedy.curso.domain.Categoria;
+import com.isaackennedy.curso.domain.Produto;
 import com.isaackennedy.curso.repositories.CategoriaRepository;
+import com.isaackennedy.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursoApplication implements CommandLineRunner {
+public class Application implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository catRepository;
+	
+	@Autowired
+	private ProdutoRepository prodRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().add(cat2);
+		
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		prodRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
