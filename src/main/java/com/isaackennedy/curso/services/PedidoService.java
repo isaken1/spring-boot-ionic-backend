f37cf403a92f7541ab205cb1d253a8bd4686cdb7
isaac_kennedy@hotmail.com <isaac_kennedy@hotmail.com>
2018-10-12 17:26:20 -0300

    Criação e disponibilização do endpoint /pedidos/{id}

---
+++ src/main/java/com/isaackennedy/curso/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.isaackennedy.curso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isaackennedy.curso.domain.Pedido;
+import com.isaackennedy.curso.repositories.PedidoRepository;
+import com.isaackennedy.curso.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> cat = repo.findById(id);
+		
+		return cat.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
