8e6e0ade992eb861abb51757dad4b4ed472e90b8
isaac_kennedy@hotmail.com <isaac_kennedy@hotmail.com>
2018-10-12 14:35:08 -0300

    Cliente, TipoCliente, telefones e enderecos finalizados

--- src/main/java/com/isaackennedy/curso/Application.java
+++ src/main/java/com/isaackennedy/curso/Application.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.isaackennedy.curso.domain.Categoria;
 import com.isaackennedy.curso.domain.Cidade;
+import com.isaackennedy.curso.domain.Cliente;
+import com.isaackennedy.curso.domain.Endereco;
 import com.isaackennedy.curso.domain.Estado;
 import com.isaackennedy.curso.domain.Produto;
+import com.isaackennedy.curso.domain.enums.TipoCliente;
 import com.isaackennedy.curso.repositories.CategoriaRepository;
 import com.isaackennedy.curso.repositories.CidadeRepository;
+import com.isaackennedy.curso.repositories.ClienteRepository;
+import com.isaackennedy.curso.repositories.EnderecoRepository;
 import com.isaackennedy.curso.repositories.EstadoRepository;
 import com.isaackennedy.curso.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class Application implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository citRepository;
+	
+	@Autowired
+	private ClienteRepository cliRepo;
+	
+	@Autowired
+	private EnderecoRepository endRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -66,5 +77,16 @@ public class Application implements CommandLineRunner {
 		prodRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estRepository.saveAll(Arrays.asList(est1, est2));
 		citRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "3877012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		cliRepo.save(cli1);
+		endRepo.saveAll(Arrays.asList(e1, e2));
 	}
 }
---
+++ src/main/java/com/isaackennedy/curso/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.isaackennedy.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.isaackennedy.curso.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
