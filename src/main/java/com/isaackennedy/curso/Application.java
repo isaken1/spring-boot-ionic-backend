3f5632f0913d2056b3792706a83c87b561efbf77
isaac_kennedy@hotmail.com <isaac_kennedy@hotmail.com>
2018-10-12 15:40:03 -0300

    Instanciação, criação de repositórios e finalização das classes Pagamento (e subclasses) e pedido

--- src/main/java/com/isaackennedy/curso/Application.java
+++ src/main/java/com/isaackennedy/curso/Application.java
@@ -1,5 +1,6 @@
 package com.isaackennedy.curso;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.isaackennedy.curso.domain.Cidade;
 import com.isaackennedy.curso.domain.Cliente;
 import com.isaackennedy.curso.domain.Endereco;
 import com.isaackennedy.curso.domain.Estado;
+import com.isaackennedy.curso.domain.Pagamento;
+import com.isaackennedy.curso.domain.PagamentoComBoleto;
+import com.isaackennedy.curso.domain.PagamentoComCartao;
+import com.isaackennedy.curso.domain.Pedido;
 import com.isaackennedy.curso.domain.Produto;
+import com.isaackennedy.curso.domain.enums.EstadoPagamento;
 import com.isaackennedy.curso.domain.enums.TipoCliente;
 import com.isaackennedy.curso.repositories.CategoriaRepository;
 import com.isaackennedy.curso.repositories.CidadeRepository;
 import com.isaackennedy.curso.repositories.ClienteRepository;
 import com.isaackennedy.curso.repositories.EnderecoRepository;
 import com.isaackennedy.curso.repositories.EstadoRepository;
+import com.isaackennedy.curso.repositories.PagamentoRepository;
+import com.isaackennedy.curso.repositories.PedidoRepository;
 import com.isaackennedy.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class Application implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository endRepo;
+	
+	@Autowired
+	private PedidoRepository pedRepo;
+	
+	@Autowired 
+	private PagamentoRepository pgtoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -88,5 +102,23 @@ public class Application implements CommandLineRunner {
 		
 		cliRepo.save(cli1);
 		endRepo.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pgto1);
+		
+		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pgto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedRepo.saveAll(Arrays.asList(ped1, ped2));
+		
+		pgtoRepo.saveAll(Arrays.asList(pgto1, pgto2));
+		
 	}
 }
