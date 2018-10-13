7727f436ca1b2306bab3553aa6e97ed50e8746e9
isaac_kennedy@hotmail.com <isaac_kennedy@hotmail.com>
2018-10-13 19:45:04 -0300

    Inserção de categoria via POST

--- src/main/java/com/isaackennedy/curso/services/CategoriaService.java
+++ src/main/java/com/isaackennedy/curso/services/CategoriaService.java
@@ -22,4 +22,8 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
+	public Categoria insert(Categoria obj) {
+		return repo.save(obj);
+	}
+	
 }
