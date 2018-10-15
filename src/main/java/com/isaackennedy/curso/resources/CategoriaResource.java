8d2836e0387d6d0d41de685c8346eaf2158d3cd5
isaac_kennedy@hotmail.com <isaac_kennedy@hotmail.com>
2018-10-15 18:54:49 -0300

    Endpoint para listagem de todas as categorias

--- src/main/java/com/isaackennedy/curso/resources/CategoriaResource.java
+++ src/main/java/com/isaackennedy/curso/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.isaackennedy.curso.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.isaackennedy.curso.domain.Categoria;
+import com.isaackennedy.curso.dto.CategoriaDTO;
 import com.isaackennedy.curso.services.CategoriaService;
 
 @RestController
@@ -46,6 +49,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping( method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
--- src/main/java/com/isaackennedy/curso/services/CategoriaService.java
+++ src/main/java/com/isaackennedy/curso/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.isaackennedy.curso.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException ex) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 }
