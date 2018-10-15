da55bad571e2a35d202e6d2936fbd325939a0b8a
isaac_kennedy@hotmail.com <isaac_kennedy@hotmail.com>
2018-10-15 19:48:54 -0300

    Validação sintática com Beans Validation

--- src/main/java/com/isaackennedy/curso/services/CategoriaService.java
+++ src/main/java/com/isaackennedy/curso/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.isaackennedy.curso.domain.Categoria;
+import com.isaackennedy.curso.dto.CategoriaDTO;
 import com.isaackennedy.curso.repositories.CategoriaRepository;
 import com.isaackennedy.curso.services.exceptions.DataIntegrityException;
 import com.isaackennedy.curso.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 }
