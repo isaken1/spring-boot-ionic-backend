d0d6b13978a2e78a61cc072adfff3657cfb69a0f
Isaac Kennedy <isaackennedy@info.ufrn.br>
2020-01-23 16:54:12 -0300

    Criação do profile dev.

---
+++ src/main/java/com/isaackennedy/curso/config/DevConfig.java
@@ -0,0 +1,30 @@
+package com.isaackennedy.curso.config;
+
+import com.isaackennedy.curso.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+        dbService.initializeTestDB();
+        return true;
+    }
+}
